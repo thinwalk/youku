3271d2c41f8689613cd2a0b9e616580462be66fa
史鹏飞 <251400931@qq.com>
2016-03-11 16:31:03 +0800

    This is Bug Fix!

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
 // This is master
 // This is master2!
 
+/*
+ * This is Bug Fix!	
+ */
+
  require "youku.class.php";
  $url = "http://v.youku.com/v_show/id_XMTM0MDIwNjkyMA==.html";
  $data = Youku::parse($url);
